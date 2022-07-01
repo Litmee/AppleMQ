0ca50bca192db570f2075c3c090b659224cdd3cb
litmee <1970432@qq.com>
2022-07-01 18:01:56 +0800

    update

--- bin/apple/run.go
+++ bin/apple/run.go
@@ -1,27 +1,31 @@
 package apple
 
+import (
+	"log"
+	"net"
+)
+
 func Run() {
 
 	// Read the apple.ini configuration file
 	readConf()
 
+	// Start the service
+	listen, err := net.Listen("tcp", "127.0.0.1:"+options["port"])
+	if err != nil {
+		panic(err)
+	}
+
 	// Check and process the value of a configuration item
 	checkAndDealOption()
 
-	//// 1. Start the service
-	//listen, err := net.Listen("tcp", "127.0.0.1:9082")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//// 2. Polling waits for the client to establish a connection
-	//for {
-	//	conn, err := listen.Accept()
-	//	if err != nil {
-	//		log.Println("accept failed, err:%V\n", err)
-	//		continue
-	//	}
-	//	// 3. Start a separate goroutine to handle the connection
-	//	go process(conn)
-	//}
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Println("accept failed, err:%V\n", err)
+			continue
+		}
+		// Start a separate goroutine to handle the connection
+		go process(conn)
+	}
 }
