e94568df42da213ee301f749b3634d49b140477b
litmee <1970432@qq.com>
2022-07-04 17:49:21 +0800

    update

--- bin/apple/deal.go
+++ bin/apple/deal.go
@@ -9,38 +9,55 @@ import (
 
 // Synchronized cluster messages
 func separate(m []byte) {
-	b, _ := treaty.Encode(string(m))
-	for _, v := range clusterMQArr {
-		_, err := v.c.Write(b)
-		if err != nil {
-			v.state = 0
-			log.Println("Message synchronization failed")
-			FailLock.Lock()
-			s, ok := failureMessageCollection[v.addr]
-			if ok {
-				s = append(s, b)
-				continue
-			}
-			s = make([][]byte, 1)
-			s[0] = b
-			failureMessageCollection[v.addr] = s
-			FailLock.Unlock()
-		}
-	}
-}
 
-func dealMessage(m []byte) {
-	globalQueue.Push(m)
 	var info message.AppleMessage
 	err := json.Unmarshal(m, &info)
 	if err != nil {
 		log.Println("message json serialization failed, err: ", err)
 		return
 	}
-	if info.Sign == 0 {
-		info.Sign = 1
-		marshal, _ := json.Marshal(info)
-		// Distribute messages to other cluster nodes
-		go separate(marshal)
+	if info.Sign != 0 {
+		return
 	}
+
+	info.Sign = 1
+	marshal, _ := json.Marshal(info)
+
+	b, _ := treaty.Encode(string(marshal))
+	for _, v := range clusterMQArr {
+		if v.c == nil {
+			goto deal
+		} else {
+			_, err := v.c.Write(b)
+			if err != nil {
+				goto deal
+			}
+			continue
+		}
+	deal:
+		v.state = 0
+		// log.Println("Message synchronization failed")
+		FailLock.Lock()
+		s, ok := failureMessageCollection[v.addr]
+		if ok {
+			s = append(s, b)
+			failureMessageCollection[v.addr] = s
+			FailLock.Unlock()
+			continue
+		}
+		s = make([][]byte, 1)
+		s[0] = b
+		failureMessageCollection[v.addr] = s
+		FailLock.Unlock()
+	}
+}
+
+func dealMessageStandalone(m []byte) {
+	globalQueue.Push(m)
+}
+
+func dealMessageCluster(m []byte) {
+	globalQueue.Push(m)
+	// Distribute messages to other cluster nodes
+	go separate(m)
 }
