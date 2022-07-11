73cc64aba562f121be08ebd255cda8c28d0cdba5
litmee <1970432@qq.com>
2022-07-11 18:23:40 +0800

    update

--- bin/message/design.go
+++ bin/message/design.go
@@ -3,6 +3,8 @@ package message
 type AppleMessage struct {
 	// Sign: 0 means from the producer, 1 means from the cluster machine synchronization
 	Sign int
+	// Body: data entity
 	Body string
-	Tag  string
+	// Tag: information label
+	Tag string
 }
